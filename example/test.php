6c7f52f58de2453abecfbf97045c1369c2509d4a
Jobs <Jobs@Jobss-MacBook-Air.local>
2016-12-14 22:19:22 +0800

    first commit

---
+++ example/test.php
@@ -0,0 +1,5 @@
+include '../src/MyLibrary.php';
+
+$obj = new  myfirst\assemble\MyLibrary();
+
+$obj -> func();
---
+++ src/MyLibrary.php
@@ -0,0 +1,8 @@
+mespace myfirst\assemble;
+
+class MyLibrary{  
+    public function func(){
+
+    echo "加载我的组件\n";
+    }
+}
